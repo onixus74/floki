47d05a2a5e25a29e6c2e583a27b3f860833039b2
Philip Sampaio Silva <philip.sampaio@gmail.com>
2019-08-28 22:07:48 -0300

    WIP: processing the tree builder @ initial mode
    This is the start of the tree builder.

--- lib/floki/html_tree/comment.ex
+++ lib/floki/html_tree/comment.ex
@@ -2,5 +2,6 @@ defmodule Floki.HTMLTree.Comment do
   @moduledoc false
 
   # Represents a comment inside an HTML tree with reference to its parent node id.
+  # TODO: rename content to data
   defstruct content: "", node_id: nil, parent_node_id: nil
 end
